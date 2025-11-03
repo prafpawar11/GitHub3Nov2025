b91f842bbe8dd6a49a8fef3b6de162b1d104b9ee
prafpawar11 <=>
2025-11-03 19:57:32 +0530

    PIM TC by Pankaj

---
+++ src/main/java/pageLayer/PimPage.java
@@ -0,0 +1,11 @@
+package pageLayer;
+
+public class PimPage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("PIM Page Code by Pankaj Automation test Engineer");
+	}
+
+}
