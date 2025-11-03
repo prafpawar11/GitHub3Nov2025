d3a4a9662579fbb4ecc1df87891c48ab1243c6cc
prafpawar11 <=>
2025-11-03 19:52:16 +0530

    Home TC by Ashiwini

---
+++ src/main/java/pageLayer/HomePage.java
@@ -0,0 +1,10 @@
+package pageLayer;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Home Page code by Ashiwini Automation Test Engineer...");
+	}
+
+}
