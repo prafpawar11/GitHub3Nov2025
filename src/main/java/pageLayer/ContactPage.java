dc4519a60d2c7381bb95c8aa14e9b2fabd70ffcd
prafpawar11 <=>
2025-11-03 20:03:43 +0530

    Contact TC by Pankaj

---
+++ src/main/java/pageLayer/ContactPage.java
@@ -0,0 +1,10 @@
+package pageLayer;
+
+public class ContactPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Contact page TC by Pankaj automation test Engineer..");
+	}
+
+}
